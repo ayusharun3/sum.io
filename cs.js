5ce1ea5cd7395bc2059a0f230e6a39ab12b8ae49
ayusharun3 <154236215+ayusharun3@users.noreply.github.com>
2023-12-19 13:03:21 +0400

    Add files via upload

---
+++ cs.js
@@ -0,0 +1,21 @@
+function addNumbers() {
+
+    var num1 = document.getElementById("num1").value;
+    var num2 = document.getElementById("num2").value;
+
+
+    num1 = parseFloat(num1);
+    num2 = parseFloat(num2);
+
+
+    if (isNaN(num1) || isNaN(num2)) {
+        alert("Please enter valid numbers");
+        return;
+    }
+
+
+    var total = num1 + num2;
+
+
+    document.getElementById("result").innerHTML = "Result: " + total;
+}
